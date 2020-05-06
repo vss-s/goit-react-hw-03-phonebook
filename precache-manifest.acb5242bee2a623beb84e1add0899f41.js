83f5533e2fae252a6964dcbfe49e33397d7b6044
vss-s <53346463+vss-s@users.noreply.github.com>
2020-05-06 18:11:15 +0300

    Updates

---
+++ precache-manifest.acb5242bee2a623beb84e1add0899f41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb56ffd52535afb39c7441d41712a029",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "b65e3658b3b60b8ca5de",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.a55694ec.chunk.css"
+  },
+  {
+    "revision": "1be683904ff7f78b8b90",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.46a83972.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.46a83972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b65e3658b3b60b8ca5de",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.bd7f5270.chunk.js"
+  },
+  {
+    "revision": "2f190d5f5a42e281cf05",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.3fd68290.js"
+  }
+]);
